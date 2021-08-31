fb5dead968de1c5f68b3c46a9a382eb9d1d7d13b
김채은 <chenni0531@gmail.com>
2021-09-01 00:26:23 +0900

    S05P21A203-31 docs: Add the 1st assignment

---
+++ Document/사전학습/김채은/1강/Driver.java
@@ -0,0 +1,24 @@
+package ssafy;
+
+import org.apache.hadoop.util.ProgramDriver;
+
+public class Driver {
+	public static void main(String[] args) {
+		int exitCode = -1;
+		ProgramDriver pgd = new ProgramDriver();
+		try {
+			pgd.addClass("wordcount", Wordcount.class, "A map/reduce program that perform word counting.");
+			pgd.addClass("wordcount1char", Wordcount1char.class, "A map/reduce program that counts 1st character of words in the input files.");
+			pgd.addClass("wordcountsort", Wordcountsort.class, "A map/reduce program that output frequency of the words in the input files by alphabetical order.");
+			pgd.addClass("inverted", Wordcountsort.class, "A map/reduce program that generates the inverted index using words in the input files.");
+			pgd.addClass("matadd", MatrixAdd.class, "A map/reduce program computes the addition of two matrices.");
+      			pgd.driver(args);
+			exitCode = 0;
+		}
+		catch(Throwable e) {
+			e.printStackTrace();
+		}
+
+		System.exit(exitCode);
+	}
+}

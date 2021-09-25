affabf7607991435bf6c400244149d038ec0a3cf
qlfflwls5 <qlfflwls2@gmail.com>
2021-09-25 23:30:51 +0900

    S05P21A203-196 feat: Add arrival-list API

--- Project/server/airlines/urls.py
+++ Project/server/airlines/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('profiles/<int:user_pk>/reviews/', views.user_review_list),
     path('logs/', views.user_log_list),
+    path('states/', views.arrival_list),
 ]

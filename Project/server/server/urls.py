16df43b03cda0d74bcf6b9092202e0d1d38e489c
qlfflwls5 <qlfflwls2@gmail.com>
2021-09-25 23:07:02 +0900

    S05P21A203-195 feat: Set swagger

--- Project/server/server/urls.py
+++ Project/server/server/urls.py
@@ -15,9 +15,32 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url 
+from rest_framework import permissions 
+from drf_yasg.views import get_schema_view 
+from drf_yasg import openapi
+
+schema_url_patterns = [ 
+    path('api/v1/', include('accounts.urls')),
+    path('api/v1/', include('airlines.urls')),
+]
+
+schema_view = get_schema_view( 
+    openapi.Info( 
+        title="Django API", 
+        default_version='v1', 
+        terms_of_service="https://www.google.com/policies/terms/", 
+    ), 
+    public=True, 
+    permission_classes=(permissions.AllowAny,), 
+    patterns=schema_url_patterns, 
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('accounts.urls')),
     path('api/v1/', include('airlines.urls')),
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'), 
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'), 
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]

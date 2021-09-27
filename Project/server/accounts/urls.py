e802a5d4b60af420e9c22fd8ccf8a576d50fc2df
subin <ansubin2004@gmail.com>
2021-09-27 15:44:59 +0900

    S05P21A203-251 refactor: Delete drf JWT code

--- Project/server/accounts/urls.py
+++ Project/server/accounts/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
 from . import views
 
-from rest_framework_jwt.views import obtain_jwt_token
-
 urlpatterns = [
     path('auth/login/', views.login),
-    path('api-token-auth/', obtain_jwt_token),
 ]

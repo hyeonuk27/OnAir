a29b0cb6c7c580c3c091711f248e6ce4d3a25e2d
qlfflwls5 <qlfflwls2@gmail.com>
2021-09-29 01:01:21 +0900

    S05P21A203-260 feat: Modify settings.py

--- Project/server/server/settings.py
+++ Project/server/server/settings.py
@@ -144,7 +144,6 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'server' / 'static',
 ]
 
 STATIC_ROOT = BASE_DIR / 'my_static'

ba02da42fec8123df87ae495ec774dc18b72cba3
김채은 <chenni0531@gmail.com>
2021-09-29 17:32:42 +0900

    S05P21A203-236 feat: Add review keyword

--- Project/server/airlines/urls.py
+++ Project/server/airlines/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('reviews/airline/<airline_id>/', views.review_list),
     path('reviews/<review_id>/', views.review_detail),
     path('reviews/score/<airline_id>/', views.review_score),
-    path('reviews/keyword/<airline_id>/', views.review_keyword),
+    path('keyword/<airline_id>/', views.review_keyword),
 ]

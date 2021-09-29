658392ce8f429a43eede97821bee80a75a32c457
hyeonuk <hyeonuk27@gmail.com>
2021-09-29 11:24:40 +0900

    S05P21A203-239 feat: Add navber

--- Project/client/src/components/page/NavBar.vue
+++ Project/client/src/components/page/NavBar.vue
@@ -1,12 +1,34 @@
 <template>
-  <div>
-    NavBar
+  <div class="d-flex justify-content-between">
+    <nav class="navbar">
+      <!-- 로고 -->
+      <button @click="moveToMain">
+        <img
+          src="@/assets/log.png"
+          style="height: 2.1rem"
+          alt="logo-image"
+        />
+      </button>
+
+      <!-- 로그인 -->
+      <button @click="moveToLogin">로그인</button>
   </div>
 </template>
 
 <script>
 export default {
-
+  name: "Navbar",
+  methods: {
+    moveToMain() {
+      if (this.$route.path !== "/") {
+        this.$router.push({ name: "Main" })
+      }
+    },
+    moveToLogin() {
+      if (this.$route.path !== "login") {
+        this.$router.push({ name: "Login" })
+      }
+    },
 }
 </script>
 

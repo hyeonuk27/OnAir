fd9f8880e4f4d8084c40eb34510395090fbfa2ae
hyeonuk <hyeonuk27@gmail.com>
2021-10-01 02:46:03 +0900

    S05P21A203-239 feat: Add review create button

--- Project/client/src/components/airline/reviews/ReviewCreateButton.vue
+++ Project/client/src/components/airline/reviews/ReviewCreateButton.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
-
+    <button @click="moveToReviewCreate">리뷰 작성</button>
   </div>
 </template>
 
 <script>
 export default {
   name: ReviewCreateButton,
+  method: {
+    moveToReviewCreate() {
+      this.$router.push({ name: "ReviewCreate" })
+    }
+  } 
+  
 }
 </script>
 

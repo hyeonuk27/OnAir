22b44a9c606a6ad46f24b0e774514de82aa98797
qlfflwls5 <qlfflwls2@gmail.com>
2021-09-30 11:07:46 +0900

    S05P21A203-264 feat: Connect AirlineElement.vue to Airline.vue

--- Project/client/src/views/Airline.vue
+++ Project/client/src/views/Airline.vue
@@ -1,11 +1,26 @@
 <template>
   <div>
+    {{arrival_id}}
+    {{airline_id}}
   </div>
 </template>
 
 <script>
 export default {
+  name: 'Airline',
+  data () {
+    return {
+      arrival_id: '',
+      airline_id: '',
+    }
+  },
+  methods: {
 
+  },
+  created() {
+    this.arrival_id = this.$route.params.arrival_id
+    this.airline_id = this.$route.params.airline_id
+  }
 }
 </script>
 

a51ea78070df74af5b987929ccbfd3e5ed10f80b
qlfflwls5 <qlfflwls2@gmail.com>
2021-10-02 00:59:34 +0900

    S05P21A203-275 feat: Change server

--- Project/client/src/common/drf.js
+++ Project/client/src/common/drf.js
@@ -1,5 +1,5 @@
-const SERVER_URL = 'http://127.0.0.1:8000/api/v1'
-// const SERVER_URL = 'https://j5a203.p.ssafy.io/api/v1'
+// const SERVER_URL = 'http://127.0.0.1:8000/api/v1'
+const SERVER_URL = 'https://j5a203.p.ssafy.io/api/v1'
 
 export default {
   URL: SERVER_URL,

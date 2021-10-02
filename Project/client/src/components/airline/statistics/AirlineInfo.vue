f0aaab0c7b3fdbcffebf88ff70ff7d57e9bc5e77
ji woo park <jiu.1997.park@gmail.com>
2021-10-02 17:35:18 +0900

    S05P21A203-263 feat: Update AirlineInfo.vue

--- Project/client/src/components/airline/statistics/AirlineInfo.vue
+++ Project/client/src/components/airline/statistics/AirlineInfo.vue
@@ -26,13 +26,13 @@
         >
       </div>
       <div class="line-intro text-start">
-        <span id="detailText" >{{ airlineInfo.detail }}</span>
+        <p>{{ airlineInfo.detail }}</p>
       </div>
-      <div class="line-info text-start">
-        <span id="detailText" class="address"><i class="fas fa-building address"></i>{{ airlineInfo.address }}</span> |
-        <span id="detailText" class="num"><i class="fas fa-phone num"></i>{{ airlineInfo.phone_number }}</span> |
-        <a :href="airlineInfo.site_url" id="detailText" class="website"><i class="fas fa-globe website"></i>홈페이지<i class="fas fa-external-link-alt website"></i></a> |
-        <a :href="airlineInfo.corona_url" id="detailText" class="covid19"><i class="fas fa-info-circle covid19"></i>코로나 19 업데이트<i class="fas fa-external-link-alt covid19"></i></a>
+      <div class="text-start line-info">
+        <p class="address"><i class="fas fa-building address"></i>{{ airlineInfo.address }}</p> |
+        <p class="num"><i class="fas fa-phone num"></i>{{ airlineInfo.phone_number }}</p> |
+        <a :href="airlineInfo.site_url" target="_blank" class="website"><i class="fas fa-globe website"></i>홈페이지<i class="fas fa-external-link-alt website"></i></a> |
+        <a :href="airlineInfo.corona_url" target="_blank" class="text-danger covid19"><i class="fas fa-info-circle website"></i>코로나 19 업데이트<i class="fas fa-external-link-alt covid19"></i></a>
       </div>
     </div>
   </div>
@@ -54,52 +54,62 @@ export default {
 
 <style>
   .airline-info {
-    width: 700px;
-    margin: 0 auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 
   .airline-info-wrapper {
     display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    grid-gap: 30px 40px;
+    grid-template-columns: 150px 20px 40px 1fr;
+    grid-gap: 0px 20px;
     grid-auto-rows: min-content;
   }
 
   .profile-logo {
     grid-column: 1;
     grid-row: 1;
-    width: 100px;
+    width: 150px;
     display: flex;
-    align-items: center;
     justify-content: center;
+    align-items: center;
   }
 
   .team-logo {
-    grid-column: 2;
+    grid-column: 2 / 3;
     grid-row: 1;
     width: 20px;
     display: flex;
-    align-items: center;
     justify-content: center;
+    align-items: center;
   }
   
   .line-intro {
-    grid-column: 3;
+    grid-column: 4;
     grid-row: 1;
-    width: 400px;
+    width: 100%;
+    line-height: 120%;
+    font-size: 12px;
+    display: flex;
+    align-items: center;
+    color: #656F8C;
   }
 
   .line-info {
     grid-column: 1 / 5;
     grid-row: 2;
-    margin: 10px;
+    display: inline;
+    line-height: 110%;
+    font-size: 12px;
+    color: #656F8C;
   }
-  
   .address {
-    padding: 5px;
+    display: inline;
+    padding-right: 5px;
   }
 
   .num {
+    display: inline;
     padding: 5px;
   }
 
@@ -113,8 +123,4 @@ export default {
     text-decoration: none;
   }
 
-  #detailText {
-    font-size: x-small;
-    line-height: 100%;
-  }
 </style>

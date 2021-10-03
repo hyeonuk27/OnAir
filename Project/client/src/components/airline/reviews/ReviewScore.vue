522854e3182f7238840f2eadfd0685ffa21be09b
subin <ansubin2004@gmail.com>
2021-10-03 20:04:11 +0900

    S05P21A203-299 feat: Update review sentiment component

--- Project/client/src/components/airline/reviews/ReviewScore.vue
+++ Project/client/src/components/airline/reviews/ReviewScore.vue
@@ -7,6 +7,37 @@
 <script>
 export default {
   name: "ReviewScore",
+  props: {
+    airlineId: String,
+  },
+  data() {
+    return {
+      seat: 0,
+      service: 0,
+      checkin: 0,
+      food: 0,
+    }
+  },
+  methods: {
+    getScore: function () {
+      axios({
+        url: `${API.URL}${API.ROUTES.reviewDetail}score/${this.airlineId}/`,
+        method: "get",
+      })
+      .then((res) => {
+        this.seat = res.data['seat_score']
+        this.service = res.data['service_score']
+        this.checkin = res.data['checkin_score']
+        this.food = res.data['food_score']
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    },
+  },
+  created() {
+    this.getScore()
+  },
 }
 </script>
 
--- Project/client/src/components/airline/reviews/ReviewSentiment.vue
+++ Project/client/src/components/airline/reviews/ReviewSentiment.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
+    <div class="progress">
+      <div class="progress-bar progress-bar-animated bg-danger" 
+        role="progressbar" aria-valuenow="75" 
+        aria-valuemin="0" aria-valuemax="100" 
+        style="width: {{ negative }}%">
+      </div>
+      <div class="progress-bar progress-bar-animated" 
+        role="progressbar" aria-valuenow="75" 
+        aria-valuemin="0" aria-valuemax="100" 
+        style="width: {{ positive }}%">
+      </div>
+  </div>
 
   </div>
 </template>
@@ -7,6 +19,34 @@
 <script>
 export default {
   name: "ReviewSentiment",
+  props: {
+    airlineId: String,
+  },
+  data() {
+    return {
+      positive: 0,
+      negative: 0,
+    }
+  },
+  methods: {
+    getSentiment: function () {
+      axios({
+        url: `${API.URL}${API.ROUTES.reviewDetail}sentiment/${this.airlineId}/`,
+        method: "get",
+      })
+      .then((res) => {
+        this.positive = res.data['positive']
+        this.negative = res.data['negative']
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    },
+
+  },
+  created() {
+    this.getSentiment()
+  },
 }
 </script>
 

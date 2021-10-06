779e12c937a70cb1de14d4106afc009a21342970
ji woo park <jiu.1997.park@gmail.com>
2021-10-06 14:59:07 +0900

    S05P21A203-310 feat: Update DB

--- Project/server/server/settings.py
+++ Project/server/server/settings.py
@@ -25,8 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = False
-DEBUG = True
+DEBUG = False
+# DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
